9ed6a1d453cfb725cbd438956fe5fde5b84ba432
cvalderrama <cvalderrama@riafinancial.com>
2017-11-24 10:19:28 -0300

    Adding error handling basedon promises to API Calls

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const request = require('request');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const rp = require('request-promise');
-//require('ssl-root-cas').inject();
+require('ssl-root-cas').inject();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+let urlApi = 'https://services.comparaonline.com/dealer';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,63 +32,58 @@ app.get('/', function (req, res) {
   if (typeof req.session.views === 'undefined') {
     req.session.views = 1;
   }
-  //if(typeof req.session.token == 'undefined'){
-  let url = `https://services.comparaonline.com/dealer/deck`
-  request({method:'POST', uri: url}, function (err, response, body)
-  {
-    //console.log(err);
-    //console.log(body);
-    if(err){
-      res.render('index', {decks: null, error: 'Error,  please try again'});
-    }
-    else
-    {
+
+  let url = `https://${urlApi}/deck`;
+
+  var options = {
+      uri: url,
+      method: 'POST',
+      headers: {
+       'User-Agent': 'Request-Promise'
+      },
+      json: false
+  };
+
+  rp(options)
+    .then(function (body) {
+      console.log(body);
       let apiKey = body;
       req.session.token = apiKey;
-
-      if(typeof req.session.token == 'undefined')
-      {
-        res.render('index', {decks: null,  error: 'Error, please try again'});
-      }
-      else
-      {
-        res.render('index');
-      }
-     }
-  });
-  //}
+      res.render('index');
+     })
+    .catch(function (err) {
+       res.render('index', {decks: null, error: 'Error,  please try again'});
+    });
 })
 
 app.post('/', function (req, res) {
   //console.log(req.body.option);
   let apiKey = req.session.token;
-  //console.log(apiKey);
-  //req.session.token = apiKey;
   let deck = 5;
-  let url = `https://services.comparaonline.com/dealer/deck/${apiKey}/deal/${deck}`;
-  //console.log(url);
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {decks: null, error: 'Error, please try again'});
-    } else {
-      //if(err)
-      let result = JSON.parse(body);
-      if(result.statusCode === 200){
-        let decksText = `Number: ${decks[0].number}, Suit: ${decks[0].suit}`;
-        res.render('index', {decks: decksText, error: null});
-      }
-      if(decks == undefined){
-        res.render('index', {decks: null, error: 'Error, please try again'});
-      } else {
-        //console.log(decks);
+  let url = `${urlApi}/${apiKey}/deal/${deck}`;
+  console.log(url);
 
-      }
-    }
-  });
+  var options = {
+      uri: url,
+      headers: {
+       'User-Agent': 'Request-Promise'
+      },
+      json: true
+  };
+
+  rp(options)
+    .then(function (body) {
+      let decks = JSON.parse(body);
+      let decksText = `Number: ${decks[0].number}, Suit: ${decks[0].suit}`;
+      res.render('index', {decks: decksText, error: null});
+     })
+    .catch(function (err) {
+        res.render('index', {decks: null, error: 'Error, please try again'});
+    });
 })
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Poker Web App listening at http://%s:%s', host, port);
 })
